5b3d1f924caae55a919757429fc40ef4f0fe9019
Bryncidently <email@brynglotfelty.com>
2024-02-08 19:04:25 -0500

    typeDefs exist

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,11 +3,38 @@ const typeDefs = `
     _id: ID
     username: String
     email: String
-    password: String
+    bookCount: Int
+    savedBooks:[Book]
   },
+  type Auth {
+    token: ID!
+    user: User
+  },
+  type Book {
+    description: String
+    title: String
+    bookId: String
+    authors:[String]
+    image: String
+    link: String
+  },
+  input BookData {
+    description: String
+    title: String
+    bookId: String
+    authors:[String]
+    image: String
+    link: String
+  }
   type Query {
-    users: [User]
- 
+    me: User
+  },
+  type Mutation {
+    addUser(name: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    saveBook(book: BookData!): User
+    removeBook(bookId: ID!): User
   }
 
   
